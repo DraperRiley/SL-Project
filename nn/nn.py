97c923227e254dada96abd2bd7d87cb084a122f1
DraperRiley <rileyldraper@gmail.com>
2022-05-07 18:02:13 -0500

    implemented backpropagation with cumulative loss

--- nn/nn.py
+++ nn/nn.py
@@ -1,5 +1,3 @@
-import xmlrpc.client
-
 import numpy as np
 import preprocess
 import matplotlib.pyplot as plt
@@ -43,8 +41,30 @@ class Loss:
         return 0.5 * square_loss_sum_final
 
 class BackPropgation:
-    def backpropogation(self):
-        pass
+    def update_output(self, lr, layer, pred, target, inputs):
+
+        delta_k = pred * (1-pred) * (target - pred)
+        delta_w = np.zeros(layer.weights.shape)
+        delta_w += delta_k
+        delta_w = lr * delta_w * inputs.T
+        bias_update = lr * delta_k
+
+        return delta_w, bias_update, delta_k
+
+    def update_hidden(self, lr, layer, forward_layer, delta_k, inputs):
+
+        delta_w = np.copy(forward_layer.weights)
+        dk_wk = delta_k * delta_w
+        k_sum = np.sum(dk_wk, axis=1)
+        delta_j = layer.d_relu(layer.output)
+        delta_j = delta_j * k_sum
+        # use xij, if no previous layer, use input data
+        delta_w = np.zeros(layer.weights.shape)
+        delta_w = delta_w + inputs.T
+        delta_w = lr * delta_j * delta_w
+        bias_update = lr * delta_j
+
+        return delta_w, bias_update, delta_j
 
 def main():
 
@@ -53,8 +73,8 @@ def main():
     IMAGE_WIDTH = 28
     IMAGE_HEIGHT = 28
     INPUTS = IMAGE_WIDTH*IMAGE_HEIGHT
-    lr = 0.1
-    iterations = 5000
+    lr = 0.05
+    iterations = 10000
 
     X = np.array([[1,0,0,0,0,0,0,0],
          [0,1,0,0,0,0,0,0],
@@ -67,10 +87,12 @@ def main():
 
     X2 = np.array([[1,0,0,0,0,0,0,0]])
 
-    layer1 = Layer(8, 3)
-    layer2 = Layer(3, 8)
+    layer1 = Layer(8, 4)
+    test_layer = Layer(4, 4)
+    layer2 = Layer(4, 8)
 
     loss_function = Loss()
+    back_prop = BackPropgation()
 
     loss = []
     x_val = []
@@ -86,27 +108,36 @@ def main():
             # print(nn_input)
 
             layer1.forward(nn_input)
-            temp = layer1.relu(layer1.output)
+            layer1_out = layer1.relu(layer1.output)
 
-            layer2.forward(temp)
+            test_layer.forward(layer1_out)
+            test_layer_out = test_layer.relu(test_layer.output)
+
+            layer2.forward(test_layer_out)
             pred = layer2.sigmoid(layer2.output)
 
             # class_targets = [0,1,2,3,4,5,6,7] We will stick to one-hot encoding
             loss_sum += loss_function.cumulative_loss(nn_input, pred)
 
             # Backpropagation for the output layer
+            '''
             delta_k = pred * (1-pred) * (nn_input - pred)
             delta_w = np.zeros(layer2.weights.shape)
             delta_w += delta_k
-            delta_w = lr * delta_w * temp.T
+            delta_w = lr * delta_w * layer1_out.T
             layer2.weights += delta_w
             layer2.biases += lr * delta_k
+            '''
+
+            delta_w, bias_update, delta_k = back_prop.update_output(lr, layer2, pred, nn_input, test_layer_out)
+            layer2.weights += delta_w
+            layer2.biases += bias_update
 
             # Backpropagation for the hidden layer NOTE: hidden layer uses ReLU
-
-            delta_w = np.copy(layer1.weights)
-            dk_wk = delta_k.T * delta_w
-            k_sum = np.sum(dk_wk, axis=0)
+            '''
+            delta_w = np.copy(layer2.weights)
+            dk_wk = delta_k * delta_w
+            k_sum = np.sum(dk_wk, axis=1)
             delta_j = layer1.d_relu(layer1.output)
             delta_j = delta_j * k_sum
             # use xij, if no previous layer, use input data
@@ -115,9 +146,17 @@ def main():
             delta_w = lr * delta_j * delta_w
             layer1.weights += delta_w
             layer1.biases += lr * delta_j
+            '''
+
+            delta_w, bias_update, delta_j = back_prop.update_hidden(lr, test_layer, layer2, delta_k, layer1_out)
+            test_layer.weights += delta_w
+            test_layer.biases += bias_update
+
+            delta_w, bias_update, delta_j = back_prop.update_hidden(lr, layer1, test_layer, delta_j, nn_input)
+            layer1.weights += delta_w
+            layer1.biases += bias_update
 
         loss.append(loss_sum)
-
 
     plt.plot(x_val, loss)
     plt.xlabel('Iteration')
@@ -126,11 +165,13 @@ def main():
     plt.show()
 
     layer1.forward(X)
-    temp = layer1.relu(layer1.output)
+    layer1_out = layer1.relu(layer1.output)
 
-    layer2.forward(temp)
+    test_layer.forward(layer1_out)
+    test_layer_out = test_layer.relu(test_layer.output)
+
+    layer2.forward(test_layer_out)
     pred = layer2.sigmoid(layer2.output)
-    print(pred, '\n')
 
     print(np.argmax(pred, axis=1))
 
---
+++ nn/nn_cat_dog.py
@@ -0,0 +1,13 @@
+import numpy as np
+import nn
+import preprocess
+import matplotlib.pyplot as plt
+
+
+def main():
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
